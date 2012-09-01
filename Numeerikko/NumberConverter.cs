9668b1c5d3b8f5b099f499cbe80a7d70ad6dfab4
Laura Leppänen <laura.leppanen@iki.fi>
2012-09-01 19:50:49 +0300

    Reformat.

--- Numeerikko/NumberConverter.cs
+++ Numeerikko/NumberConverter.cs
@@ -2,13 +2,13 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
+ * modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution. 
+ *    and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -26,149 +26,129 @@ using System.Collections.Generic;
 
 namespace Numeerikko
 {
-	public class NumberConverter
-	{
-		private static string[] numberConversions = new string[]
-		{
-			"nolla", "yksi", "kaksi", "kolme", "neljä", "viisi",
-			"kuusi", "seitsemän", "kahdeksan", "yhdeksän", "kymmenen"
-		};
-		
-		private static string[] bigNumberSuffixes = new string[]
-		{
-			"", "tuhat", "miljoona", "miljardi", "biljoona", "tuhat", "triljoona"
-		};
-		
-		public NumberConverter()
-		{
-		}
-		
-		public string Convert(ulong number)
-		{
-			if (number >= 0 && number < 1000)
-			{
-				return ConvertTripleDigits((int) number);
-			}
-			else
-			{
-				return ConvertBigNumber(number);
-			}
-		}
-		
-		private string ConvertBigNumber(ulong number)
-		{
-			string result = "";
-			List<int> groupedNumber = GroupNumber(number);
+    public class NumberConverter
+    {
+        private static string[] numberConversions = new string[]
+        {
+            "nolla", "yksi", "kaksi", "kolme", "neljä", "viisi",
+            "kuusi", "seitsemän", "kahdeksan", "yhdeksän", "kymmenen"
+        };
+        private static string[] bigNumberSuffixes = new string[]
+        {
+            "", "tuhat", "miljoona", "miljardi", "biljoona", "tuhat", "triljoona"
+        };
 
-			for (int i = groupedNumber.Count - 1; i >= 0; i--) {
-				if (ShouldPrintNumber(groupedNumber, i)) {
-					int num = groupedNumber[i];
-					if (num > 1) {
-						result += ConvertTripleDigits(num);
-						result += ShouldPrintSpaceBeforeSuffix(i) ? " " : "";
-					}
-					result += bigNumberSuffixes[i];
-					
-					if (num > 1 || HasThousandsAsModifier(groupedNumber, i)) {
-						result += InflectionalSuffixFor(i);
-					} 
-					result += " ";
-				}
-			}
-			return result.Trim();
-		}
-		
-		private string InflectionalSuffixFor(int suffixIndex)
-		{
-			if (suffixIndex == 0)
-			{
-				return "";
-			}
-			return bigNumberSuffixes[suffixIndex] == "tuhat" ? "ta" : "a";
-		}
-		
-		private bool ShouldPrintSpaceBeforeSuffix(int suffixIndex)
-		{
-			return suffixIndex > 1 && bigNumberSuffixes[suffixIndex] != "tuhat";
-		}
-		
-		private bool ShouldPrintNumber(List<int> groupedNumber, int index)
-		{
-			return groupedNumber[index] != 0 || HasThousandsAsModifier(groupedNumber, index);
-		}
-		
-		private bool HasThousandsAsModifier(List<int> groupedNumber, int index)
-		{
-			return index == 4 && groupedNumber.Count - 1 > index &&
-				groupedNumber[index+1] > 0;
-		}
-		
-		private List<int> GroupNumber(ulong number)
-		{
-			string numberString = number.ToString();
-			List<int> groups = new List<int>();
-			
-			int currentIndex = numberString.Length;
-			while (currentIndex > 0)
-			{
-				currentIndex = currentIndex - 3;
-				int groupLength = 3;
-				if (currentIndex < 0)
-				{
-					groupLength = 3 + currentIndex;
-					currentIndex = 0;
-				}
-				int numberGroup = Int32.Parse(numberString.Substring(currentIndex, groupLength));
-				groups.Add(numberGroup);
-			}
-			return groups;
-		}
-		
-		private string ConvertTripleDigits(int number)
-		{
-			if (number >= 0 && number < 100)
-			{
-				return ConvertUpToDoubleDigits(number);
-			}
-			else if (number >= 100 && number < 1000)
-			{
-				string hundred = "sata";
-				int firstDigit = number / 100;
-				int rest = number % 100;
-				
-				string hundreds = firstDigit == 1 ? hundred
-												  : numberConversions[firstDigit] + hundred + "a";
-				string doubleDigits = rest > 0 ? ConvertUpToDoubleDigits(rest) : "";
-				return hundreds + doubleDigits;
-			}
-			else
-			{
-				throw new ArgumentException("I can only convert numbers that have up to three digits.");
-			}
-		}
+        public NumberConverter ()
+        {
+        }
 
-		private string ConvertUpToDoubleDigits(int number)
-		{
-			if (number >= 0 && number <= 10)
-			{
-				return numberConversions[number];
-			}
-			else if (number > 10 && number < 20)
-			{
-				return numberConversions[number - 10] + "toista";
-			}
-			else if (number >= 20 && number < 100)
-			{
-				int firstDigit = number / 10;
-				int secondDigit = number % 10;
-				string suffix = secondDigit > 0 ? numberConversions[secondDigit] : "";
-				return numberConversions[firstDigit] + "kymmentä" + suffix;
-			}
-			else
-			{
-				throw new ArgumentException("I can only convert numbers that have up to two digits.");
-			}
-		}
-	}
+        public string Convert(ulong number)
+        {
+            if (number >= 0 && number < 1000) {
+                return ConvertTripleDigits ((int)number);
+            } else {
+                return ConvertBigNumber (number);
+            }
+        }
+
+        private string ConvertBigNumber(ulong number)
+        {
+            string result = "";
+            List<int> groupedNumber = GroupNumber (number);
+
+            for (int i = groupedNumber.Count - 1; i >= 0; i--) {
+                if (ShouldPrintNumber (groupedNumber, i)) {
+                    int num = groupedNumber [i];
+                    if (num > 1) {
+                        result += ConvertTripleDigits (num);
+                        result += ShouldPrintSpaceBeforeSuffix (i) ? " " : "";
+                    }
+                    result += bigNumberSuffixes [i];
+
+                    if (num > 1 || HasThousandsAsModifier (groupedNumber, i)) {
+                        result += InflectionalSuffixFor (i);
+                    }
+                    result += " ";
+                }
+            }
+            return result.Trim ();
+        }
+
+        private string InflectionalSuffixFor(int suffixIndex)
+        {
+            if (suffixIndex == 0) {
+                return "";
+            }
+            return bigNumberSuffixes [suffixIndex] == "tuhat" ? "ta" : "a";
+        }
+
+        private bool ShouldPrintSpaceBeforeSuffix(int suffixIndex)
+        {
+            return suffixIndex > 1 && bigNumberSuffixes [suffixIndex] != "tuhat";
+        }
+
+        private bool ShouldPrintNumber(List<int> groupedNumber, int index)
+        {
+            return groupedNumber [index] != 0 || HasThousandsAsModifier (groupedNumber, index);
+        }
+
+        private bool HasThousandsAsModifier(List<int> groupedNumber, int index)
+        {
+            return index == 4 && groupedNumber.Count - 1 > index &&
+             groupedNumber [index + 1] > 0;
+        }
+
+        private List<int> GroupNumber(ulong number)
+        {
+            string numberString = number.ToString ();
+            List<int> groups = new List<int> ();
+
+            int currentIndex = numberString.Length;
+            while (currentIndex > 0) {
+                currentIndex = currentIndex - 3;
+                int groupLength = 3;
+                if (currentIndex < 0) {
+                    groupLength = 3 + currentIndex;
+                    currentIndex = 0;
+                }
+                int numberGroup = Int32.Parse (numberString.Substring (currentIndex, groupLength));
+                groups.Add (numberGroup);
+            }
+            return groups;
+        }
+
+        private string ConvertTripleDigits(int number)
+        {
+            if (number >= 0 && number < 100) {
+                return ConvertUpToDoubleDigits (number);
+            } else if (number >= 100 && number < 1000) {
+                string hundred = "sata";
+                int firstDigit = number / 100;
+                int rest = number % 100;
+
+                string hundreds = firstDigit == 1 ? hundred
+                                               : numberConversions [firstDigit] + hundred + "a";
+                string doubleDigits = rest > 0 ? ConvertUpToDoubleDigits (rest) : "";
+                return hundreds + doubleDigits;
+            } else {
+                throw new ArgumentException ("I can only convert numbers that have up to three digits.");
+            }
+        }
+
+        private string ConvertUpToDoubleDigits(int number)
+        {
+            if (number >= 0 && number <= 10) {
+                return numberConversions [number];
+            } else if (number > 10 && number < 20) {
+                return numberConversions [number - 10] + "toista";
+            } else if (number >= 20 && number < 100) {
+                int firstDigit = number / 10;
+                int secondDigit = number % 10;
+                string suffix = secondDigit > 0 ? numberConversions [secondDigit] : "";
+                return numberConversions [firstDigit] + "kymmentä" + suffix;
+            } else {
+                throw new ArgumentException ("I can only convert numbers that have up to two digits.");
+            }
+        }
+    }
 }
-
--- NumeerikkoTest/NumeerikkoTest.cs
+++ NumeerikkoTest/NumeerikkoTest.cs
@@ -27,152 +27,152 @@ using Numeerikko;
 
 namespace NumeerikkoTest
 {
-	[TestFixture]
-	public class BasicTest
-	{
-		private NumberConverter converter;
-		
-		[SetUp]
-		public void SetUp()
-		{
-			converter = new NumberConverter();
-		}
-		
-		[Test]
-		public void TestSingleDigits()
-		{
-			Assert.That(converter.Convert(0), Is.EqualTo("nolla"));
-			Assert.That(converter.Convert(1), Is.EqualTo("yksi"));
-			Assert.That(converter.Convert(2), Is.EqualTo("kaksi"));
-			Assert.That(converter.Convert(3), Is.EqualTo("kolme"));
-			Assert.That(converter.Convert(4), Is.EqualTo("neljä"));
-			Assert.That(converter.Convert(5), Is.EqualTo("viisi"));
-			Assert.That(converter.Convert(6), Is.EqualTo("kuusi"));
-			Assert.That(converter.Convert(7), Is.EqualTo("seitsemän"));
-			Assert.That(converter.Convert(8), Is.EqualTo("kahdeksan"));
-			Assert.That(converter.Convert(9), Is.EqualTo("yhdeksän"));
-		}
-		
-		[Test]
-		public void TestHundreds()
-		{
-			Assert.That (converter.Convert(100), Is.EqualTo("sata"));
-			Assert.That (converter.Convert(200), Is.EqualTo("kaksisataa"));
-			Assert.That (converter.Convert(300), Is.EqualTo("kolmesataa"));
-			Assert.That (converter.Convert(400), Is.EqualTo("neljäsataa"));
-			Assert.That (converter.Convert(500), Is.EqualTo("viisisataa"));
-			Assert.That (converter.Convert(600), Is.EqualTo("kuusisataa"));
-			Assert.That (converter.Convert(700), Is.EqualTo("seitsemänsataa"));
-			Assert.That (converter.Convert(800), Is.EqualTo("kahdeksansataa"));
-			Assert.That (converter.Convert(900), Is.EqualTo("yhdeksänsataa"));
-		}
-		
-		[Test]
-		public void TestTens()
-		{
-			Assert.That (converter.Convert(10), Is.EqualTo("kymmenen"));
-			Assert.That (converter.Convert(20), Is.EqualTo("kaksikymmentä"));
-			Assert.That (converter.Convert(30), Is.EqualTo("kolmekymmentä"));
-			Assert.That (converter.Convert(40), Is.EqualTo("neljäkymmentä"));
-			Assert.That (converter.Convert(50), Is.EqualTo("viisikymmentä"));
-			Assert.That (converter.Convert(60), Is.EqualTo("kuusikymmentä"));
-			Assert.That (converter.Convert(70), Is.EqualTo("seitsemänkymmentä"));
-			Assert.That (converter.Convert(80), Is.EqualTo("kahdeksankymmentä"));
-			Assert.That (converter.Convert(90), Is.EqualTo("yhdeksänkymmentä"));
-		}
-		
-		[Test]
-		public void TestTeens()
-		{
-			Assert.That(converter.Convert(11), Is.EqualTo("yksitoista"));
-			Assert.That(converter.Convert(12), Is.EqualTo("kaksitoista"));
-			Assert.That(converter.Convert(13), Is.EqualTo("kolmetoista"));
-			Assert.That(converter.Convert(14), Is.EqualTo("neljätoista"));
-			Assert.That(converter.Convert(15), Is.EqualTo("viisitoista"));
-			Assert.That(converter.Convert(16), Is.EqualTo("kuusitoista"));
-			Assert.That(converter.Convert(17), Is.EqualTo("seitsemäntoista"));
-			Assert.That(converter.Convert(18), Is.EqualTo("kahdeksantoista"));
-			Assert.That(converter.Convert(19), Is.EqualTo("yhdeksäntoista"));
-		}
-		
-		[Test]
-		public void TestDoubleDigits()
-		{
-			Assert.That(converter.Convert(25), Is.EqualTo("kaksikymmentäviisi"));
-			Assert.That(converter.Convert(99), Is.EqualTo("yhdeksänkymmentäyhdeksän"));
-			Assert.That(converter.Convert(82), Is.EqualTo("kahdeksankymmentäkaksi"));
-			Assert.That(converter.Convert(64), Is.EqualTo("kuusikymmentäneljä"));
-			Assert.That(converter.Convert(71), Is.EqualTo("seitsemänkymmentäyksi"));
-			Assert.That(converter.Convert(48), Is.EqualTo("neljäkymmentäkahdeksan"));
-		}
-		
-		[Test]
-		public void TestTripleDigits()
-		{
-			Assert.That(converter.Convert(125), Is.EqualTo("satakaksikymmentäviisi"));
-			Assert.That(converter.Convert(999), Is.EqualTo("yhdeksänsataayhdeksänkymmentäyhdeksän"));
-			Assert.That(converter.Convert(821), Is.EqualTo("kahdeksansataakaksikymmentäyksi"));
-			Assert.That(converter.Convert(640), Is.EqualTo("kuusisataaneljäkymmentä"));
-			Assert.That(converter.Convert(710), Is.EqualTo("seitsemänsataakymmenen"));
-		}
-		
-		[Test]
-		public void TestThousands()
-		{
-			Assert.That(converter.Convert(1000), Is.EqualTo("tuhat"));
-			Assert.That(converter.Convert(3000), Is.EqualTo("kolmetuhatta"));
-			Assert.That(converter.Convert(4002), Is.EqualTo("neljätuhatta kaksi"));
-			Assert.That(converter.Convert(200789), Is.EqualTo("kaksisataatuhatta seitsemänsataakahdeksankymmentäyhdeksän"));
-			Assert.That(converter.Convert(121000), Is.EqualTo("satakaksikymmentäyksituhatta"));
-			Assert.That(converter.Convert(21230), Is.EqualTo("kaksikymmentäyksituhatta kaksisataakolmekymmentä"));
-			Assert.That(converter.Convert(999999), Is.EqualTo("yhdeksänsataayhdeksänkymmentäyhdeksäntuhatta yhdeksänsataayhdeksänkymmentäyhdeksän"));
-		}
-		
-		[Test]
-		public void TestMillions()
-		{
-			Assert.That(converter.Convert(1000000), Is.EqualTo("miljoona"));
-			Assert.That(converter.Convert(3000000), Is.EqualTo("kolme miljoonaa"));
-			Assert.That(converter.Convert(40000002), Is.EqualTo("neljäkymmentä miljoonaa kaksi"));
-			Assert.That(converter.Convert(1300071), Is.EqualTo("miljoona kolmesataatuhatta seitsemänkymmentäyksi"));
-		}
-		
-		[Test]
-		public void TestMilliards()
-		{
-			Assert.That(converter.Convert(1000000000), Is.EqualTo("miljardi"));
-			Assert.That(converter.Convert(3000000000), Is.EqualTo("kolme miljardia"));
-			Assert.That(converter.Convert(40000000002), Is.EqualTo("neljäkymmentä miljardia kaksi"));
-			Assert.That(converter.Convert(1010300071), Is.EqualTo("miljardi kymmenen miljoonaa kolmesataatuhatta seitsemänkymmentäyksi"));
-		}
-		
-		[Test]
-		public void TestBillions()
-		{
-			Assert.That(converter.Convert(1000000000000), Is.EqualTo("biljoona"));
-			Assert.That(converter.Convert(3000000000000), Is.EqualTo("kolme biljoonaa"));
-			Assert.That(converter.Convert(40000000000002), Is.EqualTo("neljäkymmentä biljoonaa kaksi"));
-			Assert.That(converter.Convert(1003010300071), Is.EqualTo("biljoona kolme miljardia kymmenen miljoonaa kolmesataatuhatta seitsemänkymmentäyksi"));
-			
-			string nineHundredNinetyNine = "yhdeksänsataayhdeksänkymmentäyhdeksän";
-			Assert.That(converter.Convert(999999999999999), Is.EqualTo(
-				nineHundredNinetyNine + " biljoonaa " + nineHundredNinetyNine + " miljardia " +
-				nineHundredNinetyNine + " miljoonaa " + nineHundredNinetyNine + "tuhatta " +
-				nineHundredNinetyNine));
-			Assert.That(converter.Convert(200000000000000000), Is.EqualTo("kaksisataatuhatta biljoonaa"));
-		}
-		
-		[Test]
-		public void TestTrillions()
-		{
-			Assert.That(converter.Convert(1000000000000000000), Is.EqualTo("triljoona"));
-			Assert.That(converter.Convert(3000000000000000000), Is.EqualTo("kolme triljoonaa"));
-			Assert.That(converter.Convert(1000001000000000000), Is.EqualTo("triljoona biljoona"));
-			Assert.That(converter.Convert(UInt64.MaxValue), Is.EqualTo(
-				"kahdeksantoista triljoonaa neljäsataaneljäkymmentäkuusituhatta " +
-				"seitsemänsataaneljäkymmentäneljä biljoonaa seitsemänkymmentäkolme miljardia " +
-				"seitsemänsataayhdeksän miljoonaa viisisataaviisikymmentäyksituhatta kuusisataaviisitoista"));
-		}
-	}
+    [TestFixture]
+    public class BasicTest
+    {
+        private NumberConverter converter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            converter = new NumberConverter ();
+        }
+
+        [Test]
+        public void TestSingleDigits()
+        {
+            Assert.That (converter.Convert (0), Is.EqualTo ("nolla"));
+            Assert.That (converter.Convert (1), Is.EqualTo ("yksi"));
+            Assert.That (converter.Convert (2), Is.EqualTo ("kaksi"));
+            Assert.That (converter.Convert (3), Is.EqualTo ("kolme"));
+            Assert.That (converter.Convert (4), Is.EqualTo ("neljä"));
+            Assert.That (converter.Convert (5), Is.EqualTo ("viisi"));
+            Assert.That (converter.Convert (6), Is.EqualTo ("kuusi"));
+            Assert.That (converter.Convert (7), Is.EqualTo ("seitsemän"));
+            Assert.That (converter.Convert (8), Is.EqualTo ("kahdeksan"));
+            Assert.That (converter.Convert (9), Is.EqualTo ("yhdeksän"));
+        }
+
+        [Test]
+        public void TestHundreds()
+        {
+            Assert.That (converter.Convert (100), Is.EqualTo ("sata"));
+            Assert.That (converter.Convert (200), Is.EqualTo ("kaksisataa"));
+            Assert.That (converter.Convert (300), Is.EqualTo ("kolmesataa"));
+            Assert.That (converter.Convert (400), Is.EqualTo ("neljäsataa"));
+            Assert.That (converter.Convert (500), Is.EqualTo ("viisisataa"));
+            Assert.That (converter.Convert (600), Is.EqualTo ("kuusisataa"));
+            Assert.That (converter.Convert (700), Is.EqualTo ("seitsemänsataa"));
+            Assert.That (converter.Convert (800), Is.EqualTo ("kahdeksansataa"));
+            Assert.That (converter.Convert (900), Is.EqualTo ("yhdeksänsataa"));
+        }
+
+        [Test]
+        public void TestTens()
+        {
+            Assert.That (converter.Convert (10), Is.EqualTo ("kymmenen"));
+            Assert.That (converter.Convert (20), Is.EqualTo ("kaksikymmentä"));
+            Assert.That (converter.Convert (30), Is.EqualTo ("kolmekymmentä"));
+            Assert.That (converter.Convert (40), Is.EqualTo ("neljäkymmentä"));
+            Assert.That (converter.Convert (50), Is.EqualTo ("viisikymmentä"));
+            Assert.That (converter.Convert (60), Is.EqualTo ("kuusikymmentä"));
+            Assert.That (converter.Convert (70), Is.EqualTo ("seitsemänkymmentä"));
+            Assert.That (converter.Convert (80), Is.EqualTo ("kahdeksankymmentä"));
+            Assert.That (converter.Convert (90), Is.EqualTo ("yhdeksänkymmentä"));
+        }
+
+        [Test]
+        public void TestTeens()
+        {
+            Assert.That (converter.Convert (11), Is.EqualTo ("yksitoista"));
+            Assert.That (converter.Convert (12), Is.EqualTo ("kaksitoista"));
+            Assert.That (converter.Convert (13), Is.EqualTo ("kolmetoista"));
+            Assert.That (converter.Convert (14), Is.EqualTo ("neljätoista"));
+            Assert.That (converter.Convert (15), Is.EqualTo ("viisitoista"));
+            Assert.That (converter.Convert (16), Is.EqualTo ("kuusitoista"));
+            Assert.That (converter.Convert (17), Is.EqualTo ("seitsemäntoista"));
+            Assert.That (converter.Convert (18), Is.EqualTo ("kahdeksantoista"));
+            Assert.That (converter.Convert (19), Is.EqualTo ("yhdeksäntoista"));
+        }
+
+        [Test]
+        public void TestDoubleDigits()
+        {
+            Assert.That (converter.Convert (25), Is.EqualTo ("kaksikymmentäviisi"));
+            Assert.That (converter.Convert (99), Is.EqualTo ("yhdeksänkymmentäyhdeksän"));
+            Assert.That (converter.Convert (82), Is.EqualTo ("kahdeksankymmentäkaksi"));
+            Assert.That (converter.Convert (64), Is.EqualTo ("kuusikymmentäneljä"));
+            Assert.That (converter.Convert (71), Is.EqualTo ("seitsemänkymmentäyksi"));
+            Assert.That (converter.Convert (48), Is.EqualTo ("neljäkymmentäkahdeksan"));
+        }
+
+        [Test]
+        public void TestTripleDigits()
+        {
+            Assert.That (converter.Convert (125), Is.EqualTo ("satakaksikymmentäviisi"));
+            Assert.That (converter.Convert (999), Is.EqualTo ("yhdeksänsataayhdeksänkymmentäyhdeksän"));
+            Assert.That (converter.Convert (821), Is.EqualTo ("kahdeksansataakaksikymmentäyksi"));
+            Assert.That (converter.Convert (640), Is.EqualTo ("kuusisataaneljäkymmentä"));
+            Assert.That (converter.Convert (710), Is.EqualTo ("seitsemänsataakymmenen"));
+        }
+
+        [Test]
+        public void TestThousands()
+        {
+            Assert.That (converter.Convert (1000), Is.EqualTo ("tuhat"));
+            Assert.That (converter.Convert (3000), Is.EqualTo ("kolmetuhatta"));
+            Assert.That (converter.Convert (4002), Is.EqualTo ("neljätuhatta kaksi"));
+            Assert.That (converter.Convert (200789), Is.EqualTo ("kaksisataatuhatta seitsemänsataakahdeksankymmentäyhdeksän"));
+            Assert.That (converter.Convert (121000), Is.EqualTo ("satakaksikymmentäyksituhatta"));
+            Assert.That (converter.Convert (21230), Is.EqualTo ("kaksikymmentäyksituhatta kaksisataakolmekymmentä"));
+            Assert.That (converter.Convert (999999), Is.EqualTo ("yhdeksänsataayhdeksänkymmentäyhdeksäntuhatta yhdeksänsataayhdeksänkymmentäyhdeksän"));
+        }
+
+        [Test]
+        public void TestMillions()
+        {
+            Assert.That (converter.Convert (1000000), Is.EqualTo ("miljoona"));
+            Assert.That (converter.Convert (3000000), Is.EqualTo ("kolme miljoonaa"));
+            Assert.That (converter.Convert (40000002), Is.EqualTo ("neljäkymmentä miljoonaa kaksi"));
+            Assert.That (converter.Convert (1300071), Is.EqualTo ("miljoona kolmesataatuhatta seitsemänkymmentäyksi"));
+        }
+
+        [Test]
+        public void TestMilliards()
+        {
+            Assert.That (converter.Convert (1000000000), Is.EqualTo ("miljardi"));
+            Assert.That (converter.Convert (3000000000), Is.EqualTo ("kolme miljardia"));
+            Assert.That (converter.Convert (40000000002), Is.EqualTo ("neljäkymmentä miljardia kaksi"));
+            Assert.That (converter.Convert (1010300071), Is.EqualTo ("miljardi kymmenen miljoonaa kolmesataatuhatta seitsemänkymmentäyksi"));
+        }
+
+        [Test]
+        public void TestBillions()
+        {
+            Assert.That (converter.Convert (1000000000000), Is.EqualTo ("biljoona"));
+            Assert.That (converter.Convert (3000000000000), Is.EqualTo ("kolme biljoonaa"));
+            Assert.That (converter.Convert (40000000000002), Is.EqualTo ("neljäkymmentä biljoonaa kaksi"));
+            Assert.That (converter.Convert (1003010300071), Is.EqualTo ("biljoona kolme miljardia kymmenen miljoonaa kolmesataatuhatta seitsemänkymmentäyksi"));
+
+            string nineHundredNinetyNine = "yhdeksänsataayhdeksänkymmentäyhdeksän";
+            Assert.That (converter.Convert (999999999999999), Is.EqualTo (
+             nineHundredNinetyNine + " biljoonaa " + nineHundredNinetyNine + " miljardia " +
+             nineHundredNinetyNine + " miljoonaa " + nineHundredNinetyNine + "tuhatta " +
+             nineHundredNinetyNine));
+            Assert.That (converter.Convert (200000000000000000), Is.EqualTo ("kaksisataatuhatta biljoonaa"));
+        }
+
+        [Test]
+        public void TestTrillions()
+        {
+            Assert.That (converter.Convert (1000000000000000000), Is.EqualTo ("triljoona"));
+            Assert.That (converter.Convert (3000000000000000000), Is.EqualTo ("kolme triljoonaa"));
+            Assert.That (converter.Convert (1000001000000000000), Is.EqualTo ("triljoona biljoona"));
+            Assert.That (converter.Convert (UInt64.MaxValue), Is.EqualTo (
+             "kahdeksantoista triljoonaa neljäsataaneljäkymmentäkuusituhatta " +
+             "seitsemänsataaneljäkymmentäneljä biljoonaa seitsemänkymmentäkolme miljardia " +
+             "seitsemänsataayhdeksän miljoonaa viisisataaviisikymmentäyksituhatta kuusisataaviisitoista"));
+        }
+    }
 }
 
